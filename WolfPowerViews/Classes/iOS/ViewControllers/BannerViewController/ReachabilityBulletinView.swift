b035ea3b3b0152e86f49ef582d273d6bab4f3a5f
Wolf McNally <wolf@wolfmcnally.com>
2018-03-01 15:09:20 -0800

    First commit. Moving less-used view-based classes from WolfCore.

---
+++ WolfPowerViews/Classes/iOS/ViewControllers/BannerViewController/ReachabilityBulletinView.swift
@@ -0,0 +1,23 @@
+//
+//  ReachabilityBulletinView.swift
+//  WolfCore
+//
+//  Created by Wolf McNally on 6/30/17.
+//  Copyright © 2017 WolfMcNally.com. All rights reserved.
+//
+
+import UIKit
+import WolfCore
+
+public class ReachabilityBulletinView: MessageBulletinView<ReachabilityBulletin> {
+    private typealias `Self` = ReachabilityBulletinView
+
+    public override func syncToBulletin() {
+        super.syncToBulletin()
+        if specificBulletin.flags.contains(.reachable) {
+//            skin = Self.reachableSkin
+        } else {
+//            skin = Self.unreachableSkin
+        }
+    }
+}
